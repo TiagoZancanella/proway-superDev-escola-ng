31e6adca8076621b6a69e075dfa1ccbd08e88166
Tiago Bazzi Zancanella <tiagozancanella17@gmail.com>
2025-05-29 21:51:17 -0300

    finalizaod lista de formações

--- src/app/components/formacoes/formacao-lista/formacao-lista.component.ts
+++ src/app/components/formacoes/formacao-lista/formacao-lista.component.ts
@@ -1,11 +1,86 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Formacao } from "../../../models/formacao";
+import { Router } from "@angular/router";
+import { ConfirmationService, MessageService } from "primeng/api";
+import { FormacaoService } from "../../../services/formacao.service";
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { TableModule } from "primeng/table";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-formacao-lista',
-  imports: [],
   templateUrl: './formacao-lista.component.html',
-  styleUrl: './formacao-lista.component.css'
+  styleUrls: ['./formacao-lista.component.css'],
+  imports: [ TableModule, CommonModule, ToastModule, ConfirmDialogModule,ButtonModule],
+  providers: [MessageService, ConfirmationService,]
 })
 export class FormacaoListaComponent {
+  formacao: Array<Formacao> = [];
+  carregandoFormacoes?: boolean;
 
+  constructor(
+    private router: Router,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService,
+    private formacaoService: FormacaoService
+  ) { }
+
+  ngOnInit(): void {
+    this.carregarFormacoes();
+  }
+
+  private carregarFormacoes() {
+    this.carregandoFormacoes = true;
+    this.formacaoService.obterTodos().subscribe({
+      next: (formacoes) => this.formacao = formacoes,
+      error: (erro) => console.log("Ocorreu um erro ao carregar a lista de formações: " + erro),
+      complete: () => this.carregandoFormacoes = false
+    });
+  }
+
+  redirecionarPaginaCadastro() {
+    this.router.navigate(["/formacoes/formacao-cadastro"]);
+  }
+
+  confirmarParaApagar(event: Event, id: number) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Deseja realmente apagar?',
+      header: 'CUIDADO',
+      closable: true,
+      closeOnEscape: true,
+      icon: 'pi pi-exclamation-triangle',
+      rejectButtonProps: {
+        label: 'Cancelar',
+        severity: 'secondary',
+        outlined: true,
+      },
+      acceptButtonProps: {
+        label: 'Apagar',
+      },
+      accept: () => this.apagar(id)
+    });
+  }
+
+  private apagar(id: number) {
+    this.formacaoService.apagar(id).subscribe({
+      next: () => this.apresentarMensagemApagando(),
+      error: (erro) => console.log(`Ocorreu um erro ao apagar a formação: ${erro}`)
+    });
+  }
+
+  private apresentarMensagemApagando() {
+    this.messageService.add({
+      severity: "success",
+      summary: "Sucesso",
+      detail: "Formação removida com sucesso",
+    });
+    this.carregarFormacoes();
+  }
+
+  redirecionarEditar(idFormacao: number) {
+    this.router.navigate(["/formacoes/editar/" + idFormacao]);
+  }
 }
--- src/app/services/formacao.service.ts
+++ src/app/services/formacao.service.ts
@@ -12,22 +12,22 @@ export class FormacaoService {
 
   private urlApi: string;
   constructor(private http: HttpClient) {
-    this.urlApi = "http://localhost:8000/api/formacao"
+    this.urlApi = "http://localhost:8000/api/formacao/"
   }
 
   cadastrar(formacaoCadastro: Formacao): Observable<Formacao> {
     return this.http.post<Formacao>(this.urlApi, formacaoCadastro);
   }
-  //   obterTodos(): Observable<Curso[]> {
-  //     return this.http.get<Curso[]>(this.urlApi)
-  //   }
-  //   obterPorId(id: number): Observable<Curso> {
-  //     return this.http.get<Curso>(`${this.urlApi}/${id}`);
-  //   }
-  //   apagar(id: number): Observable<any> {
-  //     return this.http.delete<any>(`${this.urlApi}/${id}`);
-  //   }
-  //   editar(id: number, cursoEditar: CursoEditar): Observable<Curso> {
-  //     return this.http.put<Curso>(`${this.urlApi}/${id}`, cursoEditar);
-  //   }
+  obterTodos(): Observable<Formacao[]> {
+    return this.http.get<Formacao[]>(this.urlApi)
+  }
+  obterPorId(id: number): Observable<Formacao> {
+    return this.http.get<Formacao>(`${this.urlApi}/${id}`);
+  }
+  apagar(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.urlApi}/${id}`);
+  }
+  // editar(id: number, formacaoEditar: FormacaoEditar): Observable<Formacao> {
+  //   return this.http.put<Formacao>(`${this.urlApi}/${id}`, formacaoEditar);
+  // }
 }
